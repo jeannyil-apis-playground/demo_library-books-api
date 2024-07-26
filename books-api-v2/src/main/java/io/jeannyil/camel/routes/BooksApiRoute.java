82feb9acd9c1e6ad5422d608fd9051f6627ce43e
Jean Nyilimbibi <jean.nyilimbibi@gmail.com>
2024-07-26 12:25:39 +0200

    Upgraded to Red Hat build of Apache Camel 4.4.x for Quarkus

--- books-api-v2/src/main/java/io/jeannyil/camel/routes/BooksApiRoute.java
+++ books-api-v2/src/main/java/io/jeannyil/camel/routes/BooksApiRoute.java
@@ -1,9 +1,9 @@
 package io.jeannyil.camel.routes;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.Collections;
 import java.util.LinkedHashSet;
