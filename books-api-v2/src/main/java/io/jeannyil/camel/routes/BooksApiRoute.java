282808ab54d9acbbf86a87060d1c9e1788d33617
Jean Nyilimbibi <jean.nyilimbibi@gmail.com>
2025-12-01 19:27:46 +0100

    Upgraded to Red Hat build of Apache Camel v4.14.x for Quarkus

--- books-api-v2/src/main/java/io/jeannyil/camel/routes/BooksApiRoute.java
+++ books-api-v2/src/main/java/io/jeannyil/camel/routes/BooksApiRoute.java
@@ -15,8 +15,6 @@ import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jsonvalidator.JsonValidationException;
 import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.model.rest.RestBindingMode;
-
 import io.jeannyil.models.Author;
 import io.jeannyil.models.BookV2;
 
@@ -55,75 +53,34 @@ public class BooksApiRoute extends RouteBuilder {
         camelctx.setUseBreadcrumb(true);
 
         // Catch unexpected exceptions
-		onException(Exception.class)
-            .handled(true)
-            .maximumRedeliveries(0)
-            .log(LoggingLevel.ERROR, logName, ">>> Caught exception: ${exception.stacktrace}")
-            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
-			.setHeader(Exchange.HTTP_RESPONSE_TEXT, constant(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase()))
-			.setHeader(Exchange.CONTENT_TYPE, constant(MediaType.TEXT_PLAIN))
+        onException(Exception.class)
+			.handled(true)
+			.maximumRedeliveries(0)
+			.log(LoggingLevel.ERROR, logName, ">>> Caught exception: ${exception.stacktrace}")
             .setHeader(DEPLOYMENT_HTTP_LOCATION_HEADER, constant("{{deployment.location}}"))
-			.setBody(simple("${exception.message}"))
-            .log(LoggingLevel.INFO, logName, ">>> OUT: headers:[${headers}] - body:[${body}]")
-        ;
+			.to("direct:common-500")
+			.log(LoggingLevel.INFO, logName, ">>> OUT: headers:[${headers}] - body:[${body}]").id("log-api-unexpected-response")
+		;
 
         onException(JsonValidationException.class)
-            .handled(true)
-            .maximumRedeliveries(0)
+			.handled(true)
+			.maximumRedeliveries(0)
 			.log(LoggingLevel.ERROR, logName, ">>> Caught exception after JSON Schema Validation: ${exception.stacktrace}")
 			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(Response.Status.BAD_REQUEST.getStatusCode()))
 			.setProperty(Exchange.HTTP_RESPONSE_TEXT, constant(Response.Status.BAD_REQUEST.getReasonPhrase()))
-            .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.TEXT_PLAIN))
-            .setHeader(DEPLOYMENT_HTTP_LOCATION_HEADER, constant("{{deployment.location}}"))
-            .setBody(simple("${exception.message}"))
-            .log(LoggingLevel.INFO, logName, ">>> OUT: headers:[${headers}] - body:[${body}]")
-        ;
+            .setProperty("errorId", constant(Response.Status.BAD_REQUEST.getStatusCode()))
+            .setProperty("errorDescription", constant(Response.Status.BAD_REQUEST.getReasonPhrase()))
+            .setProperty("errorMessage", simple("${exception.message}"))
+			.setHeader(DEPLOYMENT_HTTP_LOCATION_HEADER, constant("{{deployment.location}}"))
+			.to("direct:custom-http-error")
+			.log(LoggingLevel.INFO, logName, ">>> headers:[${headers}] - body:[${body}]")
+		;
         
-        //REST configuration with Camel Quarkus Platform HTTP component
-        restConfiguration()
-            .component("platform-http")
-            .enableCORS(true)
-            .bindingMode(RestBindingMode.off) // RESTful responses will be explicitly marshaled for logging purposes
-            .dataFormatProperty("prettyPrint", "true")
-            .contextPath("/api/v2")
-        ;
-
-        // REST endpoint for the FruitsAndLegumesApi OpenAPI specification
-        rest()
-            .produces(MediaType.APPLICATION_JSON)
-            .get("/openapi.json")
-                .id("get-oas-route")
-                .description("Gets the OpenAPI specification for this service in JSON format")
-                .to("direct:getOAS")
-        ;
-
-        // Returns the OAS
-        from("direct:getOAS")
-            .log(LoggingLevel.INFO, logName, ">>> IN: headers:[${headers}] - body:[${body}]")
-            .setHeader(Exchange.CONTENT_TYPE, constant("application/vnd.oai.openapi+json"))
-            .setBody().constant("resource:classpath:openapi/openapi.json")
-            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(Response.Status.OK.getStatusCode()))
-			.setHeader(Exchange.HTTP_RESPONSE_TEXT, constant(Response.Status.OK.getReasonPhrase()))
-            .setHeader(DEPLOYMENT_HTTP_LOCATION_HEADER, constant("{{deployment.location}}"))
-            .log(LoggingLevel.INFO, logName, ">>> OUT: headers:[${headers}] - body:[${body}]")
-        ;
-
-        // REST endpoint for the Books API
-        rest("/books")
-            .get()
-                .id("get-books-v2-route")
-                .produces(MediaType.APPLICATION_JSON)
-                .description("Gets a list of all `book-v2` entities from the inventory.")
-                // Call the getBooks-v2 route
-                .to("direct:getBooks-v2")
-            .post()
-                .id("add-new-book-v2")
-                .consumes(MediaType.APPLICATION_JSON)
-                .produces(MediaType.APPLICATION_JSON)
-                .description("Adds a new `book-v2` entity in the inventory.")
-                // Call the addNewBook-v2 route
-                .to("direct:addNewBook-v2")
-        ;
+        /**
+		 * REST DSL with OpenAPI contract first approach 
+         * Reference: https://docs.redhat.com/en/documentation/red_hat_build_of_apache_camel/4.14/html/developing_applications_with_red_hat_build_of_apache_camel_for_quarkus/rest-dsl-in-camel-quarkus#camel-quarkus-extensions-rest-dsl-contract-first
+		 */
+		rest().openApi("classpath:META-INF/openapi.yaml");
         
         // Implements the getBooks-v2 operation
         from("direct:getBooks-v2")
@@ -142,7 +99,7 @@ public class BooksApiRoute extends RouteBuilder {
         from("direct:addNewBook-v2")
             .routeId("addNewBook-v2")
             .log(LoggingLevel.INFO, logName, ">>> Received addNewBook-v2 request: ${body}")
-            .to("json-validator:json-schema/book-v2_schema.json")
+            .to("json-validator:classpath:json-schema/book-v2_schema.json")
 			.log(LoggingLevel.INFO, logName, ">>> Book-v2 JSON schema validation is successful. Processing addNewBook-v2 request...")
             .unmarshal()
                 .json(JsonLibrary.Jackson, BookV2.class)
---
+++ books-api-v2/src/main/java/io/jeannyil/camel/routes/HttpErrorRoute.java
@@ -0,0 +1,67 @@
+package io.jeannyil.camel.routes;
+
+import jakarta.ws.rs.core.Response;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+/* Routes to handle common HTTP errors
+
+/!\ The @ApplicationScoped annotation is required for @Inject and @ConfigProperty to work in a RouteBuilder. 
+	Note that the @ApplicationScoped beans are managed by the CDI container and their life cycle is thus a bit 
+	more complex than the one of the plain RouteBuilder. 
+	In other words, using @ApplicationScoped in RouteBuilder comes with some boot time penalty and you should 
+	therefore only annotate your RouteBuilder with @ApplicationScoped when you really need it. */
+public class HttpErrorRoute extends RouteBuilder {
+	
+	private static String logName = HttpErrorRoute.class.getName();
+
+	@Override
+	public void configure() throws Exception {
+		
+		/**
+		 * Route that returns the common 500-Internal-Server-Error response in JSON format
+		 */
+		from("direct:common-500")
+			.routeId("common-500-http-code-route")
+			.log(LoggingLevel.INFO, logName, ">>> IN: headers:[${headers}] - body:[${body}]")
+			.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()))
+			.setHeader(Exchange.HTTP_RESPONSE_TEXT, constant(Response.Status.INTERNAL_SERVER_ERROR.getReasonPhrase()))
+			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+			.setBody()
+				.method("errorResponseHelper", 
+						"generateErrorResponse(${headers.CamelHttpResponseCode}, ${headers.CamelHttpResponseText}, ${exception})")
+			.end()
+			.marshal().json(JsonLibrary.Jackson, true)
+			.convertBodyTo(String.class) // Stream caching is enabled on the CamelContext
+			.log(LoggingLevel.INFO, logName, ">>> OUT: headers:[${headers}] - body:[${body}]")
+		;
+		
+		/**
+		 * Route that returns a custom error response in JSON format
+		 * The following properties are expected to be set on the incoming Camel Exchange:
+		 * <br>- errorId ({@link io.jeannyil.quarkus.camel.jsonvalidation.constants.APIConstants#ERROR_ID})
+		 * <br>- errorDescription ({@link io.jeannyil.quarkus.camel.jsonvalidation.constants.APIConstants#ERROR_DESCRIPTION })
+		 * <br>- errorMessage ({@link io.jeannyil.quarkus.camel.jsonvalidation.constants.APIConstants#ERROR_MESSAGE })
+		 * <br>- httpStatusCode ({@link io.jeannyil.quarkus.camel.jsonvalidation.constants.APIConstants#HTTP_STATUS_CODE })
+		 * <br>- httpStatusMsg ({@link io.jeannyil.quarkus.camel.jsonvalidation.constants.APIConstants#HTTP_STATUS_MSG })
+		 */
+		from("direct:custom-http-error")
+			.routeId("custom-http-error-route")
+			.log(LoggingLevel.INFO, logName, ">>> IN: headers:[${headers}] - body:[${body}]")
+			.setHeader(Exchange.HTTP_RESPONSE_CODE, simple("${exchangeProperty.httpStatusCode}"))
+			.setHeader(Exchange.HTTP_RESPONSE_TEXT, simple("${exchangeProperty.httpStatusMsg}"))
+			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+			.setBody()
+				.method("errorResponseHelper", 
+						"generateErrorResponse(${exchangeProperty.errorId}, ${exchangeProperty.errorDescription}, ${exchangeProperty.errorMessage})")
+			.end()
+			.marshal().json(JsonLibrary.Jackson, true)
+			.convertBodyTo(String.class) // Stream caching is enabled on the CamelContext
+			.log(LoggingLevel.INFO, logName, ">>> OUT: headers:[${headers}] - body:[${body}]")
+		;
+	}
+
+}
